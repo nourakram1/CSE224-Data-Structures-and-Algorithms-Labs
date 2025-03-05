af86716e0d16cdfb78626e3280dd8efec4af193f
nourakram1 <nourakram94@gmail.com>
2025-03-05 20:00:10 +0200

    Initialize Maven project

---
+++ Lab1/src/test/java/Test1.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Test1 {
+    @Test
+    void additionTest() {
+        assertEquals(2 + 2, 4);
+    }
+}
