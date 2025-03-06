c2dea94f51eb70101a0eb2df12f2f7b2eb496923
nourakram1 <nourakram94@gmail.com>
2025-03-06 22:34:55 +0200

    Insertion & Selection added (TODO)

---
+++ SortingTechniques/src/main/java/sort/ComparisonBased/Deterministic/InsertionSort.java
@@ -0,0 +1,10 @@
+package sort.ComparisonBased.Deterministic;
+
+import sort.Sort;
+
+public class InsertionSort<T extends Comparable<T>> extends Sort<T> {
+    @Override
+    public void sort(T[] arr) {
+
+    }
+}
