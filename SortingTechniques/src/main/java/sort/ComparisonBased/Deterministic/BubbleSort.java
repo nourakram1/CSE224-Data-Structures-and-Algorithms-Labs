975bc723a59832953bbe12ecd768e91cd70f35e8
MahmoudMonir1221 <monirmody1221@gmail.com>
2025-03-08 18:02:15 +0200

    implement BubbleSort & MergeSort

--- SortingTechniques/src/main/java/sort/ComparisonBased/Deterministic/BubbleSort.java
+++ SortingTechniques/src/main/java/sort/ComparisonBased/Deterministic/BubbleSort.java
@@ -2,6 +2,26 @@ package sort.ComparisonBased.Deterministic;
 
 import sort.Sort;
 
+/**
+ * BubbleSort is a simple, comparison-based sorting algorithm that repeatedly
+ * steps through the list,
+ * compares adjacent elements, and swaps them if they are in the wrong order.
+ * The pass through the
+ * list is repeated until no swaps are needed, which indicates that the list is
+ * sorted.
+ * <p>
+ * * Time Complexity:
+ * - Best Case: O(n) when the array is already sorted.
+ * - Average Case: O(n²) due to the nested loop comparisons and swaps.
+ * - Worst Case: O(n²) when the array is sorted in reverse order.
+ * <p>
+ * Space Complexity:
+ * - O(1) since sorting is performed in-place without additional memory usage.
+ * <p>
+ * This implementation supports generic types by extending Comparable<T>.
+ *
+ * @param <T> The type of elements to be sorted, must implement Comparable<T>.
+ */
 
 public class BubbleSort<T extends Comparable<T>> extends Sort<T> {
 
@@ -11,5 +31,18 @@ public class BubbleSort<T extends Comparable<T>> extends Sort<T> {
 
     @Override
     public void sort(T[] arr) {
+        int size = arr.length;
+        for (int i = 0; i < size - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
+                    swap(arr, j, j + 1);
+                    addStep(arr);
+                }
+                if (!swapped) {
+                    break;
+                }
+            }
+        }
     }
 }
--- SortingTechniques/src/main/java/sort/ComparisonBased/Deterministic/MergeSort.java
+++ SortingTechniques/src/main/java/sort/ComparisonBased/Deterministic/MergeSort.java
@@ -1,5 +1,7 @@
 package sort.ComparisonBased.Deterministic;
 
+import java.lang.reflect.Array;
+
 import sort.Sort;
 
 
@@ -8,8 +10,52 @@ public class MergeSort<T extends Comparable<T>> extends Sort<T> {
     public MergeSort(boolean showSteps) {
         super(showSteps);
     }
+    
+    private void merge(T[] arr,int l,int m,int r){
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        T[] L = (T[]) Array.newInstance(arr.getClass().getComponentType(), n1);
+        T[] R = (T[]) Array.newInstance(arr.getClass().getComponentType(), n2);
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i].compareTo(R[j]) <= 0) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+
+        addStep(arr);
+    }
+    private void divide(T[] arr,int l,int r){
+        if(l<r){
+            int m = (l+r)/2;
+            divide(arr,l,m);
+            divide(arr,m+1,r);
+            merge(arr,l,m,r);
+        }
+    }
 
     @Override
     public void sort(T[] arr) {
+        divide(arr, 0, arr.length-1);
     }
 }
