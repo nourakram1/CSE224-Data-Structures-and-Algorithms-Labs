2cc0390f458c3543d666e0850328fd4f00bd6af7
Abdulrahman Ismail <es-abdelrahman.ismail2027@alexu.edu.eg>
2025-03-13 22:41:56 +0200

    Add missing documentation to SortType

--- SortingTechniques/src/main/java/sort/SortType.java
+++ SortingTechniques/src/main/java/sort/SortType.java
@@ -13,6 +13,19 @@ import sort.noncomparison.NonComparisonSortType;
  */
 public interface SortType {
 
+    /**
+     * Retrieves a sorting algorithm type based on a given code.
+     * <p>
+     * This method first attempts to retrieve a {@code ComparisonSortType} using the provided code.
+     * If the code does not correspond to a {@code ComparisonSortType}, it falls back to trying a
+     * {@code NonComparisonSortType}. If neither type matches, an {@code IllegalArgumentException}
+     * may be thrown.
+     * </p>
+     *
+     * @param code The unique identifier of the sorting algorithm.
+     * @return The corresponding {@code SortType} instance.
+     * @throws IllegalArgumentException if no matching sorting type is found.
+     */
     static SortType getSortType(int code) {
         try {
             return ComparisonSortType.getByCode(code);
