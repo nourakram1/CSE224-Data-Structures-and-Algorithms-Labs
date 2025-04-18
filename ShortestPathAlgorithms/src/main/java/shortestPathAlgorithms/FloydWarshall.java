54d0de5879ab2739fae75c2dfb4edbd360802090
Abdulrahman Ismail <es-abdelrahman.ismail2027@alexu.edu.eg>
2025-04-18 23:54:28 +0200

    Return a List<DirectedEdge> in FloydWarshall::buildShortestPath
     - Rename FloydWarshall::getShortestPath to FloydWarshall::buildShortestPath

--- ShortestPathAlgorithms/src/main/java/shortestPathAlgorithms/FloydWarshall.java
+++ ShortestPathAlgorithms/src/main/java/shortestPathAlgorithms/FloydWarshall.java
@@ -11,25 +11,25 @@ import java.util.stream.IntStream;
 
 public class FloydWarshall {
     private final int n;
-    private final int[][] predecessor;
+    private final DirectedEdge[][] incomingEdge;
     @Getter
     private final double[][] dist;
 
     public FloydWarshall(Graph graph) {
         n = graph.numberOfVertices();
         dist = new double[n][n];
-        predecessor = new int[n][n];
+        incomingEdge = new DirectedEdge[n][n];
 
         for (int i = 0; i < n; i++) {
             Arrays.fill(dist[i], Double.POSITIVE_INFINITY);
             dist[i][i] = 0;
-            predecessor[i][i] = -1;
+            incomingEdge[i][i] = null;
         }
 
         for (DirectedEdge edge : graph.edgeList()) {
             if (dist[edge.source()][edge.target()] > edge.weight()) {
                 dist[edge.source()][edge.target()] = edge.weight();
-                predecessor[edge.source()][edge.target()] = edge.source();
+                incomingEdge[edge.source()][edge.target()] = edge;
             }
         }
 
@@ -42,24 +42,34 @@ public class FloydWarshall {
                 for (int v = 0; v < n; v++) {
                     if (dist[u][v] > dist[u][k] + dist[k][v]) {
                         dist[u][v] = dist[u][k] + dist[k][v];
-                        predecessor[u][v] = predecessor[k][v];
+                        incomingEdge[u][v] = incomingEdge[k][v];
                     }
                 }
             }
         }
     }
 
-    public List<Integer> getShortestPath(int src, int dst) {
-        List<Integer> path = new ArrayList<>();
+    public List<DirectedEdge> buildShortestPath(int src, int dst) {
+        if (isPartOfNegativeCycle(src, dst)) {
+            return null;
+        }
+        List<DirectedEdge> path = new ArrayList<>();
         int current = dst;
-        while (current != -1) {
-            path.addFirst(current);
-            current = predecessor[src][current];
+        while (incomingEdge[src][current] != null) {
+            path.addFirst(incomingEdge[src][current]);
+            current = incomingEdge[src][current].source();
         }
         return path;
     }
 
     public boolean hasNegativeCycle() {
         return IntStream.range(0, n).anyMatch(i -> dist[i][i] < 0);
+    }
+
+    private boolean isPartOfNegativeCycle(int src, int dst) {
+        return IntStream.range(0, n).anyMatch(i ->
+                                                dist[i][i] < 0
+                                                && dist[src][i] < Double.POSITIVE_INFINITY
+                                                && dist[dst][i] < Double.POSITIVE_INFINITY);
     }
 }
