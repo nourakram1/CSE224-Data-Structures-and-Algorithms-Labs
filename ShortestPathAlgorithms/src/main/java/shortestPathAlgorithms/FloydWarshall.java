e18d18bcda7b9b95e4b5885beabfbe28a26ad0d2
Abdulrahman Ismail <es-abdelrahman.ismail2027@alexu.edu.eg>
2025-04-18 21:48:31 +0200

    Add Floyd-Warshall algorithm
     - Update lombok version in pom.xml

--- ShortestPathAlgorithms/src/main/java/shortestPathAlgorithms/FloydWarshall.java
+++ ShortestPathAlgorithms/src/main/java/shortestPathAlgorithms/FloydWarshall.java
@@ -2,29 +2,64 @@ package shortestPathAlgorithms;
 
 import graphDataStructure.DirectedEdge;
 import graphDataStructure.Graph;
+import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class FloydWarshall {
+    private final int n;
+    private final int[][] predecessor;
+    @Getter
     private final double[][] dist;
 
     public FloydWarshall(Graph graph) {
-        int n = graph.numberOfVertices();
+        n = graph.numberOfVertices();
         dist = new double[n][n];
+        predecessor = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             Arrays.fill(dist[i], Double.POSITIVE_INFINITY);
             dist[i][i] = 0;
+            predecessor[i][i] = -1;
         }
 
         for (DirectedEdge edge : graph.edgeList()) {
-            dist[edge.source()][edge.target()] = edge.weight();
+            if (dist[edge.source()][edge.target()] > edge.weight()) {
+                dist[edge.source()][edge.target()] = edge.weight();
+                predecessor[edge.source()][edge.target()] = edge.source();
+            }
         }
 
         computeShortestPaths();
     }
 
     protected void computeShortestPaths() {
-        //TODO
+        for (int k = 0; k < n; k++) {
+            for (int u = 0; u < n; u++) {
+                for (int v = 0; v < n; v++) {
+                    if (dist[u][v] > dist[u][k] + dist[k][v]) {
+                        dist[u][v] = dist[u][k] + dist[k][v];
+                        predecessor[u][v] = predecessor[k][v];
+                    }
+                }
+            }
+        }
+    }
+
+    public List<Integer> getShortestPath(int src, int dst) {
+        List<Integer> path = new ArrayList<>();
+        int current = dst;
+        while (current != -1) {
+            path.addFirst(current);
+            current = predecessor[src][current];
+        }
+        return path;
+    }
+
+    public boolean hasNegativeCycle() {
+        return IntStream.range(0, n).anyMatch(i -> dist[i][i] < 0);
     }
 }
